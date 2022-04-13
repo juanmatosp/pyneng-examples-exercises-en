b904e307b0c6aafe5ff194099c07e0c7b041fea1
Juan Matos <juanmatos@outlook.es>
2022-04-13 19:38:59 -0400

    test mio

--- exercises/05_basic_scripts/task_5_2.py
+++ exercises/05_basic_scripts/task_5_2.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Task 5.2
@@ -24,4 +25,27 @@ Out[1]: '11111111111111111111111111110000'
 You can then take 8 bits of the binary mask using slices and convert them to decimal.
 
 Restriction: All tasks must be done using the topics covered in this and previous chapters.
+
+
+
+IPs_ingresada = input("Favor introducir la IP: ").split("/")
+mask = IPs_ingresada[-1]
+IPs = IPs_ingresada[0]
+IPs = str(IPs).replace("'","").replace("[", "").replace("]", "")
+IPs = list(IPs.split("."))
+#print(IPs[0])
+
+#print(IPs, mask)
+
+output = """
+{0:<10} {1:<10} {2:<10} {3:<10}
+{0:08b} {1:08b} {2:08b} {3:08b} """
+
+print(output.format(int(IPs[0]), int(IPs[1]), int(IPs[2]), int(IPs[3])))
+
+print('\n', output.format(int(mask[0])))
+
+mask = IPs.split("/")
+mask = mask[-1]
+print(type(IPs), IPs, mask)
 """
